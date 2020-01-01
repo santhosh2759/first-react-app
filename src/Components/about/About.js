b86f1f29727a0341c074a07d2b8d7b7404ef81cd
santhosh2759 <santhosh2759@gmail.com>
2020-01-02 04:23:39 +0530

    updated the app about

---
+++ src/Components/about/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const about = () => {
+    return <div>
+        <h1>About this App</h1>
+        <p>App to search Github users</p>
+        <p>Version 1.0.0</p>
+    </div>
+}
+
+export default about;
