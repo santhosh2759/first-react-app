64174ab0e9f4a4240c722628a781155dc8d3814c
santhosh2759 <santhosh2759@gmail.com>
2020-01-02 04:18:46 +0530

    updated the styling for header

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const header = () =>
-        <h1 className="App-header">Github Finder</h1>
+        <nav className='navbar'><ul>
+                <li style={{float:'left', padding:'14px 16px'}}>Github Finder</li>
+                <li><Link className='link' to='/about'>About</Link></li>
+                <li><Link className='link' to='/'>Home</Link></li>
+        </ul></nav>
 
 export default header;
